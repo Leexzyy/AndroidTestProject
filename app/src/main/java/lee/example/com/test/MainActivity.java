1853d0e5ee7c09b6386dd33fe31c51e7068fdb42
leezxyy <294062541@qq.com>
2019-05-17 11:48:11 +0800

    增加了滴滴的DoraemonKit测试工具

--- app/src/main/java/lee/example/com/test/MainActivity.java
+++ app/src/main/java/lee/example/com/test/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.didichuxing.doraemonkit.DoraemonKit;
+
 public class MainActivity extends AppCompatActivity {
     private Button mBtnUI;
     private Button mBtnAct;
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         mBtnUI = findViewById(R.id.btn_ui);
         mBtnAct = findViewById(R.id.btn_act);
 
-
+        DoraemonKit.install(getApplication());
         OnClick onClick = new OnClick();
         mBtnUI.setOnClickListener(onClick);
         mBtnAct.setOnClickListener(onClick);
@@ -28,10 +30,11 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             Intent intent = null;
             switch (v.getId()){
-                case  R.id.btn_ui:
-                     intent = new Intent(MainActivity.this, UIActivity.class);
                 case  R.id.btn_act:
                     intent = new Intent(MainActivity.this, ActActivity.class);
+                    break;
+                case R.id.btn_ui:
+                    intent = new Intent(MainActivity.this,UIActivity.class);
                     break;
             }
             startActivity(intent);
